05d9fa5bda562e553e8265a683f9efcc96ebfef6
IvaDeneva <denevaiva79@gmail.com>
2025-04-10 17:40:38 +0300

    remove folder client

--- client/src/environments/environment.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-  apiUrl: 'http://localhost:3000/api',
-};
-
-
-// export const environment = {
-//   apiUrl: 'https://used-furniture-server-919793127929.europe-west4.run.app/api',
-// };
---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+// export const environment = {
+//   apiUrl: 'http://localhost:3000/api',
+// };
+
+
+export const environment = {
+  apiUrl: 'https://used-furniture-server-919793127929.europe-west4.run.app/api',
+};
