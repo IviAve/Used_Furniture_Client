c5bd841246d5add330f154c9881c008c0aafbac8
IvaDeneva <denevaiva79@gmail.com>
2025-04-14 17:30:51 +0300

    test

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,8 +1,8 @@
-export const environment = {
-  apiUrl: 'http://localhost:3000/api',
-};
-
-
 // export const environment = {
-//   apiUrl: 'https://used-furniture-server-817279306338.europe-west4.run.app/api',
+//   apiUrl: 'http://localhost:3000/api',
 // };
+
+
+export const environment = {
+  apiUrl: 'https://used-furniture-server-817279306338.europe-west4.run.app/api',
+};
